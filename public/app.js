3d5eac886b1a5dcda36ad4d6654e9785d9fb17ef
Sam DaBell <sam.dabell@gmail.com>
2017-07-31 15:47:50 +0100

    Removed cats from HTML and added them via JS

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
 var newCat = { name: "Jess", favFood: "Pizza", imgURL: "https://usercontent2.hubstatic.com/12598625_f520.jpg" }
 
+var catArray = [
+  { name: "Boba", favFood: "Sock fluff", imgURL: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg" },
+  { name: "Barnaby", favFood: "Tuna", imgURL: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg" },
+  { name: "Max", favFood: "Whiskas Temptations", imgURL: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg" },
+  newCat
+];
+
 var addCat = function(cat){
   var catUl = document.createElement("ul");
   catUl.classList.add("cat");
@@ -25,5 +32,5 @@ var addCat = function(cat){
 }
 
 window.onload = function(){
-  addCat(newCat);
+  catArray.forEach(addCat);
 };
